2dce7937d7662b4bb458a33c5de71ae2602067e8
vanijain133 <71580190+vanijain133@users.noreply.github.com>
2021-01-02 16:18:17 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,15 +2,41 @@
 //We've got some basic info about Karen's home
 //Debug the type of data provided
 //Return the types concatenated in a single variable
+function moreAboutHome(address, distanceFromTown, hasNeighbours){
+var text = typeof(address)+typeof(distanceFromTown)+typeof(hasNeighbours)
+return text
+}
+
 
 //Trial 2:
 //Check if the data given is of the right type
 //parents = String, noOfSiblings = Number, isNuclearFamily = Boolean
 
+function moreAboutKaren(parents, noOfSiblings, isNuclearFamily){
+    if(typeof(parents)=="string" && typeof(noOfSiblings)=="number" && typeof(isNuclearFamily)=="boolean"){
+    return true
+    }
+    else{
+    return false
+    }
+}
+    
+    
+
 //Trial 3:
 //Lily is suspicious about Karen's new friend
 //Karen tells her friend's age and even writes it down
 //Check which one those is not a number (NaN) and return that value
+
+function doesFriendExist(ageInText, ageInNumber) {
+    if( typeof(ageInText=='NaN'))
+    if(isNaN(ageInText)){
+    return ageInText
+    }
+    else{
+    return ageInNumber
+    }
+    }
 
 //Trial 4:
 //Lily gave Karen x sweets
@@ -36,6 +62,6 @@
 //All she wants now is for her to stop crying
 //She refuses to talk to her but Lily doesn't stop trying
 //She tries out multiple things hoping for the best
-//Take all of Lily's strategies and concatenate them to a single var
+// Take all of Lily's strategies and concatenate them to a single var
 //Seperate the strategies by a single space
 //Return the length of the complete strategy
